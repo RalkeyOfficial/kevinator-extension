65c13019dce0b0ed75e03e25080eb3d7cb13ab32
RalkeyOfficial <Ralkey@outlook.com>
2022-01-27 18:23:48 +0100

    Update content.js

--- src/content.js
+++ src/content.js
@@ -1,2 +1,11 @@
-const imageCollection = document.images;
-const image = ""
+const image = "https://raw.githubusercontent.com/RalkeyOfficial/kevinator-extension/main/src/images/IMG20220118164805.jpg"; 
+
+setInterval(function() {
+    let imageCollection = document.images;
+
+    for(img in imageCollection) {
+        if (imageCollection[img].src == "" || imageCollection[img].src == "#") continue;
+        if (imageCollection[img].src == image) continue;
+        imageCollection[img].src = image;
+    }
+}, 500);
